3ebb55ce8fd8f5166f00f24cea19edf495505bd1
saramohammadi <saramohamadikahvarin@gmail.com>
2026-08-14 10:37:56 +0330

    User registeration  and chatroom messages

--- base/models.py
+++ base/models.py
@@ -12,7 +12,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length = 250)
     description = models.TextField(null=True, blank=True)
-    #participants =
+    participants =models.ManyToManyField(User,related_name='participants', blank= True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     
@@ -26,7 +26,7 @@ class Room(models.Model):
 
 
     
-class massage(models.Model):
+class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
